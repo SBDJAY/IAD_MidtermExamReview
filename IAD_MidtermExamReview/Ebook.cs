c28cf02d538328d1c5d4931abaed043a442a0833
SBDJAY <sbdjay183@gmail.com>
2023-06-19 19:01:50 -0400

    EBook Class Implemented

--- IAD_MidtermExamReview/Ebook.cs
+++ IAD_MidtermExamReview/Ebook.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace IAD_MidtermExamReview
 {
-    internal class Ebook
+    public  class Ebook: Book
     {
+        public double FileSize { get; set; }
+        private string downloadURL;
+        public string DownloadURL 
+        { 
+            get {  return downloadURL; }
+
+            
+            set
+            {
+                if (value.StartsWith("www."))
+                {
+                    downloadURL = value;
+                }
+                else
+                {
+                    throw new ArgumentException("URL should start with www.");
+                }
+            }
+        }
+
     }
 }
