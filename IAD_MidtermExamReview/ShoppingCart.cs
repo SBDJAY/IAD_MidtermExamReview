7731e4d35ee637b91cecffdf5288f8d1ee291bdb
SBDJAY <sbdjay183@gmail.com>
2023-06-19 19:20:32 -0400

    All Shopping Cart Mehtods (Finnished) - Except GetCount()

--- IAD_MidtermExamReview/ShoppingCart.cs
+++ IAD_MidtermExamReview/ShoppingCart.cs
@@ -13,15 +13,49 @@ namespace IAD_MidtermExamReview
 
         public void AddBook(Book book)
         {
+            if (!AlreadyInCart(book.Title))
+            {
+                cart.Add(book);
+                Console.WriteLine($"Book '{book.Title}' added to the cart.");
+            }
+            else
+            {
+                Console.WriteLine($"Book '{book.Title}' is already in the cart.");
+            }
         }
 
-        public void AlreadyInCart(string title)
+        private bool AlreadyInCart(string title)
         {
+            foreach (Book book in cart)
+            {
+                if (book.Title == title)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
-        public void GetCount() { }
+        public void GetCount() 
+        {
+            //return cart.Count;
+        }
 
-        public void GetTotalPRice() { }
+        public double GetTotalPRice(double discountPercentage)
+        {
+            double totalPrice = 0.0;
+
+            foreach (Book book in cart)
+            {
+                double bookPrice = book.BasePrice + book.ExtraCost;
+                totalPrice += bookPrice;
+            }
+
+            double discountAmount = totalPrice * discountPercentage;
+            double discountedTotalPrice = totalPrice - discountAmount;
+
+            return discountedTotalPrice;
+        }
 
     }
 }
