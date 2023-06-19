d490a550df2064f54e60188b6c80c21f35527dee
SBDJAY <sbdjay183@gmail.com>
2023-06-19 19:13:25 -0400

    All Shoppong Cart Methods (unfinnished)

--- IAD_MidtermExamReview/ShoppingCart.cs
+++ IAD_MidtermExamReview/ShoppingCart.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,19 @@ namespace IAD_MidtermExamReview
 {
     public class ShoppingCart
     {
+        private List <Book> cart = new List<Book> ();  
+
+        public void AddBook(Book book)
+        {
+        }
+
+        public void AlreadyInCart(string title)
+        {
+        }
+
+        public void GetCount() { }
+
+        public void GetTotalPRice() { }
+
     }
 }
