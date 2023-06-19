a019bf30983b676801c11c46eac705914dbb5ef3
SBDJAY <sbdjay183@gmail.com>
2023-06-19 18:26:55 -0400

    Add project files.

---
+++ IAD_MidtermExamReview/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IAD_MidtermExamReview
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public bool IsHardCover { get; set; }
+        public double BasePrice { get; set; }
+        public double ExtraCost { get; set; }
+
+    }
+}
---
+++ IAD_MidtermExamReview/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace IAD_MidtermExamReview;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
